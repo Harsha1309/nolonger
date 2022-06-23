50b54ae9c1240f84994ada08acde3abf24461703
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2022-06-23 21:33:48 +0530

    api exposed

--- server/config/sendBalancemail.ts
+++ server/config/sendBalancemail.ts
@@ -1,22 +1,13 @@
-const nodemailer = require("nodemailer");
-import { OAuth2Client } from "google-auth-library";
 import { IBalance } from "./interface";
-const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
-
-const CLIENT_ID = `${process.env.MAIL_CLIENT_ID}`;
-const CLIENT_SECRET = `${process.env.MAIL_CLIENT_SECRET}`;
-const REFRESH_TOKEN = `${process.env.MAIL_REFRESH_TOKEN}`;
-const SENDER_MAIL = `${process.env.SENDER_EMAIL_ADDRESS}`;
 const formData = require("form-data");
 const Mailgun = require("mailgun.js");
-
+const API_KEY = process.env.NODEMON_PRIVATE_API;
+const DOMAIN = process.env.NODEMON_DOMAIN;
 const sendBalancemail = async (
   mobilenumber: string,
   withdraw: number,
   balance: IBalance
 ) => {
-  const API_KEY = "d74b6832fc682bc2d1ba5faa95a5e0da-523596d9-c177f734";
-  const DOMAIN = "pediageek.com";
   const mailgun = new Mailgun(formData);
   const client = mailgun.client({ username: "api", key: API_KEY });
 
--- server/config/sendMail.ts
+++ server/config/sendMail.ts
@@ -1,67 +1,10 @@
-const nodemailer = require("nodemailer");
-import { OAuth2Client } from "google-auth-library";
-
-const OAUTH_PLAYGROUND = "https://developers.google.com/oauthplayground";
-
-const CLIENT_ID = `${process.env.MAIL_CLIENT_ID}`;
-const CLIENT_SECRET = `${process.env.MAIL_CLIENT_SECRET}`;
-const REFRESH_TOKEN = `${process.env.MAIL_REFRESH_TOKEN}`;
-const SENDER_MAIL = `${process.env.SENDER_EMAIL_ADDRESS}`;
 const formData = require("form-data");
 const Mailgun = require("mailgun.js");
-// send mail
+
+const API_KEY = process.env.NODEMON_PRIVATE_API;
+const DOMAIN = process.env.NODEMON_DOMAIN;
+
 const sendEmail = async (to: string, url: string, txt: string) => {
-  // const oAuth2Client = new OAuth2Client(
-  //   CLIENT_ID,
-  //   CLIENT_SECRET,
-  //   OAUTH_PLAYGROUND
-  // );
-
-  // oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-  // try {
-  //   const access_token = await oAuth2Client.getAccessToken();
-
-  //   const transport = nodemailer.createTransport({
-  //     service: "gmail",
-  //     auth: {
-  //       type: "OAuth2",
-  //       user: SENDER_MAIL,
-  //       clientId: CLIENT_ID,
-  //       clientSecret: CLIENT_SECRET,
-  //       refreshToken: REFRESH_TOKEN,
-  //       access_token,
-  //     },
-  //   });
-
-  //   const mailOptions = {
-  //     from: SENDER_MAIL,
-  //     to: to,
-  //     subject: "PediaGeek",
-  // html: `
-  //         <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-  //         <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the <b>PEDIA<span style="color:green;">GEEK</span></b>.</h2>
-  //         <p>Congratulations! You're almost set to start using <b>PEDIA<span style="color:green;">GEEK</span></b>.
-  //             Just click the button below to validate your email address.
-  //         </p>
-
-  //         <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
-
-  //         <p>If the button doesn't work for any reason, you can also click on the link below:</p>
-
-  //         <div>${url}</div>
-  //         </div>
-  //       `,
-  //   };
-
-  //   const result = await transport.sendMail(mailOptions);
-  //   return result;
-  // } catch (err) {
-  //   console.log(err);
-  // }
-  const API_KEY = "d74b6832fc682bc2d1ba5faa95a5e0da-523596d9-c177f734";
-  const DOMAIN = "pediageek.com";
-
   const mailgun = new Mailgun(formData);
   const client = mailgun.client({ username: "api", key: API_KEY });
   console.log(to);
@@ -96,3 +39,52 @@ const sendEmail = async (to: string, url: string, txt: string) => {
 };
 
 export default sendEmail;
+
+// const oAuth2Client = new OAuth2Client(
+//   CLIENT_ID,
+//   CLIENT_SECRET,
+//   OAUTH_PLAYGROUND
+// );
+
+// oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+// try {
+//   const access_token = await oAuth2Client.getAccessToken();
+
+//   const transport = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       type: "OAuth2",
+//       user: SENDER_MAIL,
+//       clientId: CLIENT_ID,
+//       clientSecret: CLIENT_SECRET,
+//       refreshToken: REFRESH_TOKEN,
+//       access_token,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: SENDER_MAIL,
+//     to: to,
+//     subject: "PediaGeek",
+// html: `
+//         <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
+//         <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the <b>PEDIA<span style="color:green;">GEEK</span></b>.</h2>
+//         <p>Congratulations! You're almost set to start using <b>PEDIA<span style="color:green;">GEEK</span></b>.
+//             Just click the button below to validate your email address.
+//         </p>
+
+//         <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
+
+//         <p>If the button doesn't work for any reason, you can also click on the link below:</p>
+
+//         <div>${url}</div>
+//         </div>
+//       `,
+//   };
+
+//   const result = await transport.sendMail(mailOptions);
+//   return result;
+// } catch (err) {
+//   console.log(err);
+// }
