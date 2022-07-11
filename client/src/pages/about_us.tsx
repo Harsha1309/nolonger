f936b196ae1a5162bfe5852e92cc76b9ec10d964
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2022-07-11 08:06:59 +0530

    abbout page changed

--- client/src/pages/about_us.tsx
+++ client/src/pages/about_us.tsx
@@ -5,117 +5,85 @@ const About = () => {
     <div className="d-flex justify-content-center my-3" style={{ width: '100%' }}>
       <Helmetglobal title="About us" description="Pediageeek is the place to express your ideas and views." keyword="aboutus,pediageek" />
       <div style={{ maxWidth: '800px' }}>
-        <h2 style={{ textAlign: "center" }}> Welcome to PediaGeek.</h2>
-        <h4 style={{ textAlign: "center" }}>
-          The best way to express your idea to the World
-        </h4>
-        <p>We are happy that you want to know something more about PediaGeek.</p>
-        <hr />
-        <div>
-          <ol>
-            <li>
-              <a href="#sec1">Introduction</a>
-            </li>
-            <li>
-              <a href="#sec2">Mission</a>
-            </li>
-            <li>
-              <a href="#sec3">About PediaGeek</a>
-            </li>
-            <li>
-              <a href="#sec4">Services</a>
-            </li>
-            <li>
-              <a href="#sec5">Founder Words</a>
-            </li>
-          </ol>
-        </div>
-        <hr />
 
-        <p id="sec1">
-          Nowadays people are more dependent on online products and services
-          that's why We have also taken a step to keep people updated in the
-          Variety of Blogs field with fresh content of Variety of Blogs. our
-          priority is to provide you better updates and information from Variety
-          of Blogs.
+        <h1 style={{ textAlign: "center" }}>Welcome to PediaGeek&nbsp;</h1>
+        <p style={{ textAlign: "center" }}>
+          <b>PediaGeek is the best way to express your ideas to the World.</b>
         </p>
-        <h2 id="sec2">our mission</h2>
-        <p>
-          There are hundreds of websites created every second, and many get dead
-          end every second and the major cause for this dead-end is the fake and
-          invaluable content they provide. So, our main goal is to provide you
-          100% Original, Safe, fresh, and useful content in Variety of Blogs.
-          which is most important to have a great experience on the world wide web
-          and internet era.
+        <p style={{ textAlign: "left" }}>
+          We are happy that you want to know something more about PediaGeek.&nbsp;
         </p>
-        <p>
-          We mainly focus on our service so and improving it regularly to provide
-          a better user experience to all users.
+        <p style={{ textAlign: "left" }}>1. Introduction&nbsp;</p>
+        <p style={{ textAlign: "left" }}>2. Mission&nbsp;</p>
+        <p style={{ textAlign: "left" }}>3. About PediaGeek&nbsp;</p>
+        <p style={{ textAlign: "left" }}>4. Services&nbsp;</p>
+        <p style={{ textAlign: "left" }}>5. Founder Words&nbsp;</p>
+        <p style={{ textAlign: "left" }}>
+          Nowadays people are more dependent on online products and services that's
+          why We have also taken a step to keep people updated with the Variety of
+          Blogs fields with fresh content of the Variety of Blogs. Our priority is to
+          provide to you with better updates and information from the Variety of
+          Blogs.
         </p>
-        <p>
-          We focus on the Variety of Blogs niche so, our main priority is to
-          search for new content and present it in front of you to make you learn
-          something new and bring out your better version.{" "}
+        <h2 style={{ textAlign: "left" }}>Our Mission&nbsp;</h2>
+        <p style={{ textAlign: "left" }}>
+          There are hundreds of websites created every second, and many meet a dead
+          end every second the major cause for this dead-end is the fake and
+          invaluable content that they provide. So, our main goal is to provide you
+          100% original, safe, fresh, and useful content in the Variety of Blogs,
+          which is most important to have a great experience in the world wide web and
+          internet era. So, we mainly focus on our service and improving it regularly
+          to provide a better user experience to all the users. We focus on the
+          Variety of Blogs niche, so, our main priority is to search for new content
+          and present it in front of you to make you learn something new and bring out
+          your better version.
         </p>
-        <h2 id="sec3">services</h2>
-        <p>
-          We are mainly focused on the Variety of Blogs categories so, We have
-          tried to proceed with your most relevant content in the mentioned
-          direction if you are interested towards Variety of Blogs then you can
-          visit daily to get more latest information.
+        <h2 style={{ textAlign: "left" }}>Services&nbsp;</h2>
+        <p style={{ textAlign: "left" }}>
+          We are mainly focused on the Variety of Blogs category. So, we have tried to
+          proceed with your most relevant content in the mentioned direction. If you
+          are interested in a Variety of Blogs then you can visit daily to get more
+          latest information. Also, We provide a Notification update service, so that
+          you can join by email and other Social Media Platforms. You can get all the
+          links on the contact page of our website if you visit now.
         </p>
-        <p>
-          Also, We provide a Notification update service you can join by email and
-          other Social Media Platforms, and all Links you can get on the contact
-          page of our website visit now.
+        <h2 style={{ textAlign: "left" }}>About PediaGeek</h2>
+        <p style={{ textAlign: "left" }}>
+          As you can see We have already mentioned what our goal and service are.
+          Again, We repeat that We mainly focus on the Variety of Blog Categories to
+          help people.PediaGeek is dedicated to providing you the best, with a focus
+          on dependability and Creating your space on the internet without much
+          hesitation. This Website is created only to help people because many people
+          are still spending hours to get exact information. So, this is the only
+          motive to create PediaGeek. To help people and provide them a better web
+          experience. Now, it is time for some words from the PediaGeek founder-
         </p>
-        <h2 id="sec4">About PediaGeek</h2>
-        <p>
-          As you can see already We mention what is our goal and Service again We
-          repeat that We mainly focus on Variety of Blogs Categories to help
-          people.PediaGeek is dedicated to providing you the it"s best, with a
-          focus on dependability and Create your space in internet without much
-          Hesitation.
-        </p>
-        <p>
-          This Website is Created only to help people because many people are
-          still spending hours to get exact information so, this is the only
-          motive to create <a href="pediageek.com">PediaGeek</a>, to help people
-          and provide them a better web experience.{" "}
-        </p>
-        <p id="sec5">Now it's the time for some words from Cname founder</p>
-        <p>
-          As per my point of view, many people visit the internet to get some
-          information but 90% of the time they get wrong information so, the
+        <p style={{ textAlign: "left" }}>
+          From my point of view, many people visit the internet to get some
+          information but 90% of the time they get the wrong information, so, the
           priority of our website PediaGeek is to provide 100% legit and accurate
-          information to our users, Also, We hope our dream comes true one day,
-          and our website will provide Original Content to provide a better user
-          experience. So, From my Side thanks for visiting our website.
+          information to our users. Also, We hope our dream comes true one day, and
+          our website will provide Original Content to provide a better user
+          experience. So, from my side, thanks for visiting our website.
         </p>
-        <p style={{ textAlign: "right" }}> (Founder PediaGeek)</p>
-        <h2 id="sec6">Contact</h2>
-        <p>
-          This website is only for you so, anything you don't like please mention
-          to us through the comment section. Contact us through the mentioned
-          link.
+        <p style={{ textAlign: "left" }}>&nbsp;(Founder PediaGeek)</p>
+        <h2 style={{ textAlign: "left" }}>Contact&nbsp;</h2>
+        <p style={{ textAlign: "left" }}>
+          This website is only for you, so, anything you don't like please mention to
+          us through the comment section. Contact us through the mentioned link. If
+          you require any more information or have any questions about PediaGeek,
+          please feel free to contact us. Team PediaGeek is working to turn its
+          passion into a booming online website. We hope you enjoy our services as
+          much as We enjoy offering them to you. We will keep posting more important
+          posts on our Website for all of you. Please give your support and love.
         </p>
-        <p>
-          If you require any more information or have any questions about our
-          about PediaGeek, please feel free to contact us.
-        </p>
+        <h4 style={{ textAlign: "left" }}>
+          <div style={{ textAlign: "center" }}>
+            Thank you for Visiting our Creation.&nbsp;
+          </div>
+          <div style={{ textAlign: "center" }}>Have a nice day.</div>
+        </h4>
 
-        <p>
-          Team PediaGeek is working to turn its passion into a booming online
-          website. We hope you enjoy our services as much as We enjoy offering
-          them to you.
-        </p>
-        <p>
-          We will keep posting more important posts on my Website for all of you.
-          Please give your support and love.
-        </p>
-        <br />
-        <p style={{ textAlign: "center" }}>Thanks For Visiting our Creation.</p>
-        <h5 style={{ textAlign: "center" }}>Have a nice day.</h5>
       </div>
     </div>
   );
